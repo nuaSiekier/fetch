581356af05902c8efea24b4288894e7f78636587
nuaSiekier <48563878+nuaSiekier@users.noreply.github.com>
2019-03-25 19:03:56 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-
+window.onload = function () {
+    showPeople();
+};
+function showPeople() 
+{
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://swapi.co/api/people/');
+    xhr.addEventListener('load', function () 
+    {
+        var test = JSON.parse(this.responseText);
+        for (var i = 0; i < test.results.length; i++) 
+        {
+            console.log(test.results[i]);
+        }
+    });
+    xhr.send();}
